65b7f6954c1a76b6c047cbda22523cf5e30d686b
g.isowa <g.isowa@gmail.com>
2021-05-24 07:19:23 +0900

    add jigyo2

--- src/components/header.js
+++ src/components/header.js
@@ -11,24 +11,29 @@ const Header = ({ siteTitle }) => (
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
+
       <NavItem href="/osirase">
-          <Nav.Link as={Link} activeClassName="active" to="/osirase">
-            重要なお知らせ
+          <Nav.Link as={Link}   activeClassName="active" to="/osirase">
+            <section className="juyo">重要なお知らせ
+              </section>
           </Nav.Link>
         </NavItem>
+
         <NavItem href="/about">
           <Nav.Link as={Link} activeClassName="active" to="/information">
             インフォメーション
           </Nav.Link>
         </NavItem>
-        <NavItem href="/about">
-          <Nav.Link as={Link} activeClassName="active" to="/about">
-            クリニック沿革
-          </Nav.Link>
-        </NavItem>
+
         <NavItem href="/about">
           <Nav.Link as={Link} activeClassName="active" to="/jigyo">
             診療内容
+          </Nav.Link>
+        </NavItem>
+        
+        <NavItem href="/about">
+          <Nav.Link as={Link} activeClassName="active" to="/about">
+            クリニック沿革
           </Nav.Link>
         </NavItem>
         
@@ -37,11 +42,13 @@ const Header = ({ siteTitle }) => (
             院長挨拶
           </Nav.Link>
         </NavItem>
+
         <NavItem href="/about">
           <Nav.Link as={Link} activeClassName="active" to="/contact">
-            お問い合わせ
+            アクセス・お問い合わせ
           </Nav.Link>
         </NavItem>
+
       </Nav>
     </Navbar.Collapse>
   </Navbar>
---
+++ src/pages/jigyou2.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Col, Row, Table } from "react-bootstrap";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="isowa" />
+    <Row>
+      <Col className="space"></Col>
+    </Row>
+    <Row>
+      <Col className="title-obi">
+        <h1 className="h1-font">診察時間</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col className="space"></Col>
+    </Row>
+    <Row>
+      <Col>
+        <Table striped bordered hover>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>月</td>
+              <td>火</td>
+              <td>水</td>
+              <td>木</td>
+              <td>金</td>
+              <td>土</td>
+              <td>日・祝</td>
+            </tr> 
+            <tr>
+              <td>9:30-12:30</td>
+              <td>◯</td>
+              <td>◯</td>
+              <td>◯</td>
+              <td>✖️</td>
+              <td>◯</td>
+              <td>◯</td>
+              <td>✖️</td>
+            </tr>
+            <tr>
+              <td>17:00-19:30</td>
+              <td>◯</td>
+              <td>◯</td>
+              <td>◯ </td>
+              <td>✖️</td>
+              <td>◯ </td>
+              <td>✖️</td>
+              <td>✖️</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+    <Row>
+      <Col className="space"></Col>
+    </Row>
+  </Layout>
+);
+
+export default AboutPage;
